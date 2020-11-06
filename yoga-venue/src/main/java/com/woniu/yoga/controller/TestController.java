e1c27077a353bf5f96f7234d215fb2afdd635545
chrn <854243450@qq.com>
2020-11-06 18:10:28 +0800

    123

---
+++ yoga-venue/src/main/java/com/woniu/yoga/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.woniu.yoga.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    public static void main(String[] args) {
+        System.out.println("123123");
+    }
+}
